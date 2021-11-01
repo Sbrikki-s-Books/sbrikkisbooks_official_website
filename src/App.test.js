dcf064aa61a09c0d8840e7c52e9298d6a4a8e3f1
Michele <michele00.pulvirenti@gmail.com>
2021-11-01 19:45:11 +0100

    Fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './components/home/home';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Home />);
+  const linkElement = screen.getByText(/Join us/i);
   expect(linkElement).toBeInTheDocument();
 });
