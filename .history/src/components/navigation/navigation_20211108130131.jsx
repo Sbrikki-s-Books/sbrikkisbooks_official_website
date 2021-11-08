99982b8dd0d688cdd6d26ceb36a561860611407b
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2021-11-08 13:03:17 +0100

    Fixed media queries

--- .history/src/components/navigation/navigation_20211108130131.jsx
+++ .history/src/components/navigation/navigation_20211108130131.jsx
@@ -5,12 +5,12 @@ import * as styles from './navigation.module.scss'
 function Navigation() {
     return (
         <>
-            <div className={styles.navDesktop}>
-                    <NavItem text="Read" link="/read" />
-                    <NavItem text="Alcool" link="/alcool" />
-                    <NavItem text="Pfizer" link="/pfizer" />
-            </div>
-            <div className={styles.navMobile}>
+        <div className={styles.navDesktop}>
+                <NavItem text="Read" link="/read" />
+                <NavItem text="Alcool" link="/alcool" />
+                <NavItem text="Pfizer" link="/pfizer" />
+        </div>
+        <div className={styles.navMobile}>
                 <NavItem text="Read" link="/read" />
                 <NavItem text="Alcool" link="/alcool" />
                 <NavItem text="Pfizer" link="/pfizer" />
