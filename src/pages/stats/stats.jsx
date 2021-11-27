b8a29ca6d72765f4d199704b2dca8aa85bb6b70f
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2021-11-27 15:57:49 +0100

    Trying to fix SEO problems

--- src/pages/stats/stats.jsx
+++ src/pages/stats/stats.jsx
@@ -4,8 +4,7 @@ import * as styles from './stats.module.scss'
 const Stats = () => {
     return (
         <div className={styles.tmp}>
-            <h1>We'll be here soon</h1>
-            <p>Sbrikki's Books SEO score</p>
+            <h1>Sbrikki's Books SEO score</h1>
             <a className={styles.link} href="https://freetools.seobility.net/en/seocheck/check?url=https%3A%2F%2Fmike-cheek.github.io%2Fsbrikkisbooks_official_website%2F&crawltype=1">
                 CLICK HERE TO SEE OUR SCORE
             </a>
