6f32e248f6559611c517b62a35e40c65b46ae717
Michele Pulvirenti <62357072+Mike-cheek@users.noreply.github.com>
2021-12-01 01:04:40 +0100

    Update navigation.jsx

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -6,16 +6,16 @@ const Navigation = (props) => {
     return (
         <>
             <div className={styles.navDesktop}>
-                <NavItem text="Home" path="/sbrikkisbooks_official_website/" />
-                <NavItem text="Spotify" path="/sbrikkisbooks_official_website/spotify" />
-                <NavItem text="Stats" path="/sbrikkisbooks_official_website/stats" />
-                <NavItem text="King" path="/sbrikkisbooks_official_website/king" />
+                <NavItem text="Home" path="/sbrikkisbooks/" />
+                <NavItem text="Spotify" path="/sbrikkisbooks/spotify" />
+                <NavItem text="Stats" path="/sbrikkisbooks/stats" />
+                <NavItem text="King" path="/sbrikkisbooks/king" />
             </div>
             <div className={styles.navMobile}>
-                <NavItem text="Home" path="/sbrikkisbooks_official_website/" onClick={props.onClick} />
-                <NavItem text="Spotify" path="/sbrikkisbooks_official_website/spotify" onClick={props.onClick} />
-                <NavItem text="Stats" path="/sbrikkisbooks_official_website/stats" onClick={props.onClick} />
-                <NavItem text="King" path="/sbrikkisbooks_official_website/king" onClick={props.onClick} />
+                <NavItem text="Home" path="/sbrikkisbooks/" onClick={props.onClick} />
+                <NavItem text="Spotify" path="/sbrikkisbooks/spotify" onClick={props.onClick} />
+                <NavItem text="Stats" path="/sbrikkisbooks/stats" onClick={props.onClick} />
+                <NavItem text="King" path="/sbrikkisbooks/king" onClick={props.onClick} />
             </div>
         </>
     )
