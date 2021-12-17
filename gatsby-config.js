05de754db3a57dfe3fdc610d1f01c1ab619aaca1
Mike-cheek <michele00.pulvirenti@gmail.com>
2021-12-17 21:16:37 +0100

    Added sitemap generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const siteUrl = process.env.URL || `https://Mike-cheek.github.io/sbrikkisbooks`
+
 module.exports = {
   pathPrefix: "/sbrikkisbooks",
   siteMetadata: {
@@ -38,6 +40,54 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+            nodes {
+              ... on WpPost {
+                uri
+                modifiedGmt
+              }
+              ... on WpPage {
+                uri
+                modifiedGmt
+              }
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allWpContentNode: { nodes: allWpNodes },
+        }) => {
+          const wpNodeMap = allWpNodes.reduce((acc, node) => {
+            const { uri } = node
+            acc[uri] = node
+
+            return acc
+          }, {})
+
+          return allPages.map(page => {
+            return { ...page, ...wpNodeMap[page.path] }
+          })
+        },
+        serialize: ({ path, modifiedGmt }) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt,
+          }
+        },
+      },
     },
   ],
 };
