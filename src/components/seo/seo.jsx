f7974087db69dd34951500ffd28db59a50c5ad45
Mike-cheek <michele00.pulvirenti@gmail.com>
2021-12-18 18:18:47 +0100

    Added lang property

--- src/components/seo/seo.jsx
+++ src/components/seo/seo.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { useLocation } from "@reach/router";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Seo = ({ title, description, image, article }) => {
-  const { pathname } = useLocation()
-  const { site } = useStaticQuery(query)
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
 
   const {
     defaultTitle,
@@ -15,66 +15,70 @@ const Seo = ({ title, description, image, article }) => {
     siteUrl,
     defaultImage,
     twitterUsername,
-  } = site.siteMetadata
+  } = site.siteMetadata;
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
     image: `${siteUrl}${image || defaultImage}`,
     url: `${siteUrl}${pathname}`,
-  }
+  };
 
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+    <html lang="en">
+      <Helmet title={seo.title} titleTemplate={titleTemplate}>
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
 
-      {seo.url && <meta property="og:url" content={seo.url} />}
+        {seo.url && <meta property="og:url" content={seo.url} />}
 
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+        {(article ? true : null) && (
+          <meta property="og:type" content="article" />
+        )}
 
-      {seo.title && <meta property="og:title" content={seo.title} />}
+        {seo.title && <meta property="og:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
+        {seo.description && (
+          <meta property="og:description" content={seo.description} />
+        )}
 
-      {seo.image && <meta property="og:image" content={seo.image} />}
+        {seo.image && <meta property="og:image" content={seo.image} />}
 
-      <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" />
 
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
+        {twitterUsername && (
+          <meta name="twitter:creator" content={twitterUsername} />
+        )}
 
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
+        {seo.title && <meta name="twitter:title" content={seo.title} />}
 
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
+        {seo.description && (
+          <meta name="twitter:description" content={seo.description} />
+        )}
 
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
-    </Helmet>
-  )
-}
-
+        {seo.image && <meta name="twitter:image" content={seo.image} />}
+      </Helmet>
+    </html>
+  );
+};
 
 Seo.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
   image: PropTypes.string,
   article: PropTypes.bool,
-}
+};
 
 Seo.defaultProps = {
-    title: "Sbrikki's Books - A Book Club for everyone",
-  description: "Sbrikki's Books is an apolitical book club for everyone. The main aim is the growth of every member from a cultural point of view. We also drink.",
+  title: "Sbrikki's Books - A Book Club for everyone",
+  description:
+    "Sbrikki's Books is an apolitical book club for everyone. The main aim is the growth of every member from a cultural point of view. We also drink.",
   image: null,
   article: false,
-}
+};
 
 const query = graphql`
-query Seo {
+  query Seo {
     site {
       siteMetadata {
         defaultTitle: title
@@ -85,7 +89,6 @@ query Seo {
       }
     }
   }
-    
-  `
+`;
 
-  export default Seo
+export default Seo;
