968e7623c04ed47d5a326cae7ba7a275340d168d
Mike-cheek <michele00.pulvirenti@gmail.com>
2021-12-22 22:43:09 +0100

    Adjusted footer

--- src/atoms/logo/logo.jsx
+++ src/atoms/logo/logo.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import logo from '../../assets/sbrikkislogoextended.svg'
 import * as styles from './logo.module.scss'
 
-const Logo = () => {
+const Logo = ({width = 100}) => {
     return (
         <a href="/" title='Go to Home Page'>
             <img 
                 src={logo} 
                 alt="Sbrikki's Books Logo"
                 className={styles.logo}
-                width={"100px"}
+                width={width + "px"}
                 height={"auto"}
                 title="Sbrikki's Books Logo"
             />
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import ContactUs from '../contactUs/contactUs'
 import * as styles from './footer.module.scss'
 import spotify from '../../assets/spotify.svg'
+import Logo from '../../atoms/logo/logo';
+import Separator from '../../atoms/separator/separator';
 
 function Footer(props) {
 
@@ -18,27 +20,29 @@ function Footer(props) {
     }
 
     return (
-        <div className={styles.footer} id="contactUs">
-            <p>
-                Join us!
-            </p>
-            <button onClick={openForm} className={styles.formButton}>
-                CONTACT US!
-            </button>
-            {formOpened ?
-                <ContactUs closeForm={closeForm} formOpened={formOpened} />
-                : null
-            }
-            <a href={"https://open.spotify.com/playlist/3XWNoTPdXdIRapbK86Y11l"} title={"Go to our playlist"}>
-                <img 
-                    src={spotify}
-                    width={'40px'}
-                    height={'40px'}
-                    alt={"Sbrikki's Books Spotify"}
-                />
-            </a>
-            
-
+        <div className={styles.footer}>
+            <Logo width={200} />
+            <Separator />
+            <div className={styles.subFooter} id="contactUs">
+                <p>
+                    Join us!
+                </p>
+                <button onClick={openForm} className={styles.formButton}>
+                    CONTACT US!
+                </button>
+                {formOpened ?
+                    <ContactUs closeForm={closeForm} formOpened={formOpened} />
+                    : null
+                }
+                <a href={"https://open.spotify.com/playlist/3XWNoTPdXdIRapbK86Y11l"} title={"Go to our playlist"}>
+                    <img 
+                        src={spotify}
+                        width={'40px'}
+                        height={'40px'}
+                        alt={"Sbrikki's Books Spotify"}
+                    />
+                </a>
+            </div>
         </div>
     )
 }
