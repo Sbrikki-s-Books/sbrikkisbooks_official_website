ee3f0bfeeb5167b9c9a5a8a9c5f096bccb1d3e7b
Mike-cheek <michele00.pulvirenti@gmail.com>
2021-12-24 12:45:18 +0100

    Fixed fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,6 @@ module.exports = {
       options: {
         fonts: [
           `Merriweather`,
-          'Roboto',
           //`source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
         display: 'swap'
